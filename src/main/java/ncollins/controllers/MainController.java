d4accc1df595c702315f80f2f45207b81238230a
Nick Collins <nickcollins24@gmail.com>
2019-06-25 17:55:35 -0700

    dockerizing

--- src/main/java/ncollins/controllers/MainController.java
+++ src/main/java/ncollins/controllers/MainController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController("/")
 public class MainController {
     @GetMapping
-    public String helloWorld() {
-        return "Hello World!";
+    public String entryPoint() {
+        return "ok";
     }
 }
