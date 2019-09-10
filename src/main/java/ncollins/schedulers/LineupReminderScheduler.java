91301f1a1625f1c2bd688303919d7d75cdb8eec1
Nick Collins <nickcollins24@gmail.com>
2019-09-09 23:19:18 -0700

    implemented WeeklyRoundupScheduler

--- src/main/java/ncollins/schedulers/LineupReminderScheduler.java
+++ src/main/java/ncollins/schedulers/LineupReminderScheduler.java
@@ -26,7 +26,7 @@ public class LineupReminderScheduler implements Scheduler {
 
         // schedule task every Thursday at 12PM PST
         Long startTime = LocalDateTime.now(TimeZone.getTimeZone("PST").toZoneId()).until(
-                LocalDate.now(TimeZone.getTimeZone("PST").toZoneId()).with(TemporalAdjusters.nextOrSame(DayOfWeek.THURSDAY)).atTime(10,0), ChronoUnit.MINUTES);
+                LocalDate.now(TimeZone.getTimeZone("PST").toZoneId()).with(TemporalAdjusters.next(DayOfWeek.THURSDAY)).atTime(10,0), ChronoUnit.MINUTES);
         scheduler.scheduleAtFixedRate(task, startTime, TimeUnit.DAYS.toMinutes(7), TimeUnit.MINUTES);
     }
 }
